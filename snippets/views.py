227f043eb59cdd3cc9cf5d80105da8f77b8ab7b6
ngnetboy <ngnetboy@163.com>
2018-03-21 16:52:09 +0800

    forth lesson

--- snippets/views.py
+++ snippets/views.py
@@ -6,13 +6,16 @@ from rest_framework.renderers import JSONRenderer
 from rest_framework.parsers import JSONParser
 """
 from snippets.models import Snippet
-from snippets.serializers import SnippetSerializer
+from snippets.serializers import SnippetSerializer, UserSerializer
 from django.http import Http404
 from rest_framework.views import APIView
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework import mixins
 from rest_framework import generics
+from django.contrib.auth.models import User
+from rest_framework import permissions
+from snippets.permissions import IsOwnerOrReadOnly
 
 
 class SnippetList1(APIView):
@@ -87,6 +90,20 @@ class SnippetDetail2(mixins.RetrieveModelMixin,
 class SnippetList(generics.ListCreateAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
 class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly,)
+
+class UserList(generics.ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+class UserDetail(generics.RetrieveAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
--- tutorial/urls.py
+++ tutorial/urls.py
@@ -21,3 +21,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     url(r'^', include('snippets.urls')),
 ]
+
